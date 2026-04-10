b59535c6e28cabbff5996c3142ad67ae3ee0d3c9
Aryan Bharadwaj <bharadwajaryan1@gmail.com>
2026-04-10 21:21:35 +0530

    Cart Api Executed

---
+++ Backend/routes/cart.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const Cart = require("../models/cart");
+
+// GET CART
+router.get("/:userId", async (req, res) => {
+  const cart = await Cart.findOne({ userId: req.params.userId });
+  res.json(cart || { items: [] });
+});
+
+// SAVE CART
+router.post("/", async (req, res) => {
+  const { userId, items } = req.body;
+
+  let cart = await Cart.findOne({ userId });
+
+  if (cart) {
+    cart.items = items;
+  } else {
+    cart = new Cart({ userId, items });
+  }
+
+  await cart.save();
+  res.json(cart);
+});
+
+module.exports = router;
